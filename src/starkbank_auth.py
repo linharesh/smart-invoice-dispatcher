4f7e609a4de51e68e71f8cd44b5cb8481e2fd167
Henrique Linhares <linhares.h@gmail.com>
2025-01-14 16:20:28 -0300

    Fix get_user method definition by adding self parameter

--- src/starkbank_auth.py
+++ src/starkbank_auth.py
@@ -6,7 +6,7 @@ import starkbank
 class StarkBankAuth:
     
     
-    def get_user():
+    def get_user(self):
         private_key_file = "private_key.pem"
         if not os.path.exists(private_key_file):
             logging.error(f"Private key file not found: {private_key_file}")
