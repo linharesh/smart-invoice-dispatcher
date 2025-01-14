7739f48c0fefce942384b76430295cd33ad0f2d8
Henrique Linhares <linhares.h@gmail.com>
2025-01-14 14:25:39 -0300

    Remove unused private key example and streamline transfer creation method

--- src/transfer.py
+++ src/transfer.py
@@ -1,38 +1,10 @@
 import starkbank
-from datetime import datetime
 
-
-
-# This is only an example of a private key content. You should use your own key.
-private_key_content = """
------BEGIN EC PRIVATE KEY-----
-
------END EC PRIVATE KEY-----
-"""
-
-# for project users:
-user = starkbank.Project(
-    environment="sandbox",
-    id="5415638830940160",
-    private_key=private_key_content
-)
-
-print(user)
-starkbank.user = user
-
-import json
-
-with open("invoice.json", 'r') as file:
-    inv = json.load(file)
-
-amount = 10000 #inv["event"]["log"]["invoice"]["amount"]
 
 class TransferCreator:
 
-    def create(self, amount, name="Stark Bank S.A.", tax_id="20.018.183/0001-80", bank_code="20018183", 
-               branch_code="0001", account_number="6341320293482496",account_type="payment"):
-        
-
+    def create(self, amount, name="Stark Bank S.A.", tax_id="20.018.183/0001-80", bank_code="20018183",
+               branch_code="0001", account_number="6341320293482496", account_type="payment"):
 
         transfer = starkbank.transfer.create(
             [
